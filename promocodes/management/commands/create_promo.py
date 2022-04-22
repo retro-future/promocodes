32b8ac55286f19eeee4280eec8aa09919adab123
darken <shad1297@mail.com>
2022-04-22 14:13:56 +0500

    create, add and delete codes

--- promocodes/management/commands/create_promo.py
+++ promocodes/management/commands/create_promo.py
@@ -1,23 +1,60 @@
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
+import json
+from os.path import exists
 
 
-class Command(BaseCommand):
-    help = 'Create random users'
+class PromoCode:
+    def __init__(self, amount: int, group_name: str):
+        self.amount = amount
+        self._group_name = group_name
+        self._code_list = []
+        self.__generate_codes()
 
-    def add_arguments(self, parser):
-        parser.add_argument('total', type=int, help='Indicates the number of users to be created')
-        # Optional argument
-        parser.add_argument('-p', '--prefix', type=str, help='Define a username prefix', )
+    def __generate_codes(self):
+        for i in range(self.amount):
+            self.codes.append(get_random_string(length=8))
 
-    def handle(self, *args, **kwargs):
-        total = kwargs['total']
-        prefix = kwargs['prefix']
+    @property
+    def group(self):
+        return self._group_name
 
-        for i in range(total):
-            if prefix:
-                username = '{prefix}_{random_string}'.format(prefix=prefix, random_string=get_random_string(length=8))
-            else:
-                username = get_random_string(length=8)
-            User.objects.create_user(username=username, email='', password='123')
+    @property
+    def codes(self):
+        return self._code_list
+
+
+class PromoCodesContext:
+    def __init__(self):
+        try:
+            with open("data_file.json") as fp:
+                self.json_data = json.load(fp)
+        except FileNotFoundError:
+            self.json_data = {}
+
+    def add(self, promo_code: PromoCode):
+        group = promo_code.group
+        codes = promo_code.codes
+        self.json_data[group] = codes
+        with open("data_file.json", "w") as fp:
+            json.dump(self.json_data, fp)
+
+    def delete(self, promo_code: PromoCode):
+        group = promo_code.group
+        if not self.json_data.get(group):
+            return "that group not exists"
+        del self.json_data[group]
+        with open("data_file.json", "w") as fp:
+            json.dump(self.json_data, fp)
+
+
+
+mypromo = PromoCode(10, "test34")
+mypromo1 = PromoCode(10, "test12")
+mypromo2 = PromoCode(10, "test23")
+
+mypromocodes = PromoCodesContext()
+mypromocodes.add(mypromo)
+mypromocodes.add(mypromo1)
+mypromocodes.add(mypromo2)
+mypromocodes.delete(mypromo2)
